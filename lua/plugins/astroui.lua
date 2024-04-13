fc64ab1a2171a3f3e1715d820b89cddd7b37859d
cooperstandard <cooperstandard@users.noreply.github.com>
2024-04-12 22:10:35 -0600

    themes and mini.surround

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,4 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
@@ -12,6 +11,8 @@ return {
   opts = {
     -- change colorscheme
     colorscheme = "astrodark",
+    -- colorscheme = "melange",
+    -- colorscheme = "monokai_soda",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
