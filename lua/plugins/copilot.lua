32121cd8f7c85c3c5b8dec111a6e72e34333cb6a
Cooper Standard <cooper@cooperstandard.org>
2026-04-22 22:59:50 -0700

    fix copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,6 @@
 return {
-"https://github.com/github/copilot.vim"
+  "https://github.com/github/copilot.vim",
+  -- vim.keymap.set("i", "C-g", 'copilot#Accept("<CR>")', { desc = "accept copilot suggestion" }),
+  vim.api.nvim_set_keymap("i", "<C-g>", 'copilot#Accept("<CR>")', { expr = true, silent = true }),
+  -- TODO: add a toggle for copilot
 }
